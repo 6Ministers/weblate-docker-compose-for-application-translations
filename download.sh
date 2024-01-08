3b7ef3a7f45e0c4a20f9f20e4ab2f67002c076e5
6ministers <manyatovskiy@gmail.com>
2024-01-08 14:52:55 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 DESTINATION=$1
 
 # clone Flectra directory
-git clone --depth=1 https://github.com/WeblateOrg/docker-compose $DESTINATION
+git clone --depth=1 https://github.com/6Ministers/weblate-docker-compose-for-application-translations $DESTINATION
 rm -rf $DESTINATION/.git
 
 
